70bfd83eb3c1593551cd780e5d6387e376bf52b7
Dave <aguirredavid28.000@gmail.com>
2026-01-08 22:50:24 -0500

    :sparkles: Creación de la carpeta config y la clase de configuración SpacesConfig

---
+++ src/main/java/com/davidag/gestion_beneficio/config/SpacesConfig.java
@@ -0,0 +1,36 @@
+package com.davidag.gestion_beneficio.config;
+
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+public class SpacesConfig {
+
+    @Bean
+    public S3Client s3client(
+        @Value("${do.spaces.key}") String key,
+            @Value("${do.spaces.secret}") String secret,
+            @Value("${do.spaces.endpoint}") String endpoint,
+            @Value("${do.spaces.region}") String region
+
+    ){
+         return S3Client.builder()
+                .endpointOverride(URI.create(endpoint))
+                .region(Region.of(region))
+                .credentialsProvider(
+                        StaticCredentialsProvider.create(
+                                AwsBasicCredentials.create(key, secret)
+                        )
+                )
+                .build();
+    }
+    
+}
